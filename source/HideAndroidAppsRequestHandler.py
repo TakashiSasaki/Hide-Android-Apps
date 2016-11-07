48094dfe601c6c4c7e7d2f9c30bf1f76b63b1c8e
Takashi SASAKI <takashi316@gmail.com>
2016-11-07 22:16:07 +0900

    implemented update button for disabled_packages

--- source/HideAndroidAppsRequestHandler.py
+++ source/HideAndroidAppsRequestHandler.py
@@ -5,7 +5,7 @@ import time
 import urllib.parse
 import webbrowser
 
-import main
+import Adb
 
 
 class HideAndroidAppsRequestHandler(http.server.SimpleHTTPRequestHandler):
@@ -13,29 +13,35 @@ class HideAndroidAppsRequestHandler(http.server.SimpleHTTPRequestHandler):
         request = urllib.parse.urlparse(self.path)
         query = request.query
         params = urllib.parse.parse_qs(query)
-        body = None
+        json_string = None
+
         if "prop" in params.keys():
-            adb = main.Adb()
+            adb = Adb.Adb()
             prop = adb.getProp()
             json_string = json.dumps(prop)
+
+        if "disabled_packages" in params.keys():
+            adb = Adb.Adb()
+            disabled_packages = adb.listDisabledPackages()
+            json_string = json.dumps(disabled_packages)
+
+        if json_string is not None:
             callback_function_name = params["callback"][0]
             body = callback_function_name + "(" + json_string + ");"
-
-        if body is not None:
             self.send_response(200)
             self.send_header('Content-type', 'text/html; charset=utf-8')
             self.send_header('Content-length', len(body))
             self.end_headers()
             self.wfile.write(body.encode())
-
-        http.server.SimpleHTTPRequestHandler.do_GET(self)
+        else:
+            http.server.SimpleHTTPRequestHandler.do_GET(self)
 
 
 class HideAndroidAppsHttpServer(http.server.HTTPServer):
     def __init__(self):
         http.server.HTTPServer.__init__(self, ("", 10000), HideAndroidAppsRequestHandler)
         self.toBeShutdown = False
-        self.timeout = 10
+        self.timeout = 120
 
     def handle_timeout(self):
         http.server.HTTPServer.handle_timeout(self)
