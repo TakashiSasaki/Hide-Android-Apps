16d448d727f1d8df7cabcafa9273bd7d625c7c1b
Takashi SASAKI <takashi316@gmail.com>
2016-11-14 19:54:00 +0900

    implemented "copy system packages" button

--- source/js/system_packages.js
+++ source/js/system_packages.js
@@ -23,3 +23,20 @@ function callbackSystemPackages(json_object){
     }
     document.getElementById("number_of_system_packages").value = json_object.length;
 }
+
+text_to_copy = "";
+
+function copySystemPackages(){
+    var table_element = document.getElementById("system_packages");
+    var model = document.getElementById("model").value;
+    for(var i=0; i<table_element.children.length; ++i){
+        var tr_element = table_element.children[i];
+        //alert(tr_element);
+        text_to_copy += tr_element.children[1].textContent + "\t\t" + model + "\ts\n";
+    }
+    window.addEventListener("copy", function(e){
+        //alert(e.clipboardData);
+        e.clipboardData.setData("text", text_to_copy);
+    });
+    document.execCommand("copy");
+}
