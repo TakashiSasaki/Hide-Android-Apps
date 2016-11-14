a905463faf9d69a2639854bcf2a1747b2637ed12
Takashi SASAKI <takashi316@gmail.com>
2016-11-14 19:57:26 +0900

    implemented "copy third party packages" button

--- source/js/third_party_packages.js
+++ source/js/third_party_packages.js
@@ -24,6 +24,19 @@ function callbackThirdPartyPackages(json_object){
     document.getElementById("number_of_third_party_packages").value = json_object.length;
 }
 
+text_to_copy = "";
+
 function copyThirdPartyPackages(){
-    alert("not implemented");
+    var table_element = document.getElementById("third_party_packages");
+    var model = document.getElementById("model").value;
+    for(var i=0; i<table_element.children.length; ++i){
+        var tr_element = table_element.children[i];
+        //alert(tr_element);
+        text_to_copy += tr_element.children[1].textContent + "\t\t" + model + "\t3\n";
+    }
+    window.addEventListener("copy", function(e){
+        //alert(e.clipboardData);
+        e.clipboardData.setData("text", text_to_copy);
+    });
+    document.execCommand("copy");
 }
