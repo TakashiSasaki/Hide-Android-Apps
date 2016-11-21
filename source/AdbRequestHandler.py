63735a81725c2e63c191aa14054a9ba664ea9f28
Takashi SASAKI <takashi316@gmail.com>
2016-11-21 23:57:38 +0900

    implemented getUninstalledPackageList

--- source/AdbRequestHandler.py
+++ source/AdbRequestHandler.py
@@ -43,11 +43,6 @@ class AdbRequestHandler(http.server.SimpleHTTPRequestHandler):
                     callback_function_name = x + "Callback"
                 json_string = self.__getattribute__(x)(params)
 
-        if "uninstalled_packages" in params.keys():
-            adb = Adb.Adb()
-            uninstalled_packages = adb.listUninstalledPackages()
-            json_string = json.dumps(uninstalled_packages)
-
         if "system_packages" in params.keys():
             adb = Adb.Adb()
             packages = adb.listSystemPackages()
@@ -122,3 +117,7 @@ class AdbRequestHandler(http.server.SimpleHTTPRequestHandler):
     def getInstalledPackageList(self, param):
         packages = self.adb.listInstalledPackages()
         return json.dumps(packages)
+
+    def getUninstalledPackageList(self, param):
+        uninstalled_packages = self.adb.listUninstalledPackages()
+        return json.dumps(uninstalled_packages)
