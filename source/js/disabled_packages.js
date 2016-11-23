df24d9590cc5a0091a794e0f6295c09d35065880
Takashi SASAKI <takashi316@gmail.com>
2016-11-23 23:54:40 +0900

    refactoring disabled_packages.js to use functions in index.js

--- source/js/disabled_packages.js
+++ source/js/disabled_packages.js
@@ -5,27 +5,30 @@ function updateDisabledPackages(){
     }
     //window.scrollTo(0, table_element.getBoudingClientRect().top + window.pageYOffset);
     document.getElementById("number_of_disabled_packages").value="updating";
-    var script_element = document.createElement("script");
-    script_element.src = "/?getDisabledPackageList=1";
-    document.body.appendChild(script_element);
+    requestJsonP("getDisabledPackageList");
+    //var script_element = document.createElement("script");
+    //script_element.src = "/?getDisabledPackageList=1";
+    //document.body.appendChild(script_element);
 }
 
 function getDisabledPackageListCallback(json_object){
     var table_element = document.getElementById("disabled_packages");
     for(var k in json_object) {
         var tr_element = document.createElement("tr");
-        var td_element_k = document.createElement("td");
-        td_element_k.textContent = k;
-        tr_element.appendChild(td_element_k);
-        var td_element_v = document.createElement("td");
-        td_element_v.textContent = json_object[k];
-        tr_element.appendChild(td_element_v);
-        var td_element_checkbox = document.createElement("td");
-        var input_element = document.createElement("input");
-        input_element.type="checkbox";
-        td_element_checkbox.appendChild(input_element);
-        tr_element.appendChild(td_element_checkbox);
-        table_element.appendChild(tr_element);
+        //var td_element_k = document.createElement("td");
+        //td_element_k.textContent = k;
+        //tr_element.appendChild(td_element_k);
+        tr_element.appendChild(createTdElementWithTextContext(k));
+        tr_element.appendChild(createTdElementWithCheckbox());
+        //var td_element_v = document.createElement("td");
+        //td_element_v.textContent = json_object[k];
+        //tr_element.appendChild(td_element_v);
+        //var td_element_checkbox = document.createElement("td");
+        //var input_element = document.createElement("input");
+        //input_element.type="checkbox";
+        //td_element_checkbox.appendChild(input_element);
+        //tr_element.appendChild(td_element_checkbox);
+        //table_element.appendChild(tr_element);
     }
     document.getElementById("number_of_disabled_packages").value=json_object.length;
 }
