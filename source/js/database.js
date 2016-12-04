266bca5a1746be00eda4fc33c3e73a5d8e9a6344
Takashi SASAKI <takashi316@gmail.com>
2016-12-04 19:59:21 +0900

    fix bug in database downloading

--- source/js/database.js
+++ source/js/database.js
@@ -2,7 +2,7 @@ function updateDatabase(){
     var model = document.getElementById("model").value;
     //alert(model);
     var database_url = "https://script.google.com/macros/s/AKfycbwujUj5du8-Om7N2-1DaIqlpeoyB6cDyKx_tkuToM4d2OiKOjnX/exec";
-    database_url += "&callback_function_name=updateDatabaseCallback";
+    database_url += "?callback_function_name=updateDatabaseCallback";
     requestJsonP2(database_url);
 }
 
